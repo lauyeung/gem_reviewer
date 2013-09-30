8db44386a301807b76ca075e29f6f8ec3c98db47
Louise Au Yeung <louise@eabx.org>
2013-09-30 14:35:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GemReviewer::Application.config.secret_key_base = 'bf602b88ce0833109df857ac801e266a0b91c64b564d1a18529d137cd810e45306a48a103f412f6bc03731d36912942c86bb2bdda733a963571f98e78ca1a107'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GemReviewer::Application.config.session_store :cookie_store, key: '_gem_reviewer_session'
