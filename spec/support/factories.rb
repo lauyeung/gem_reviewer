3b8cc4f2e3cc0949736b51a16fbf003534129d6f
Alex Phinizy <AlexPhinizy@gmail.com>
2013-10-01 16:29:19 -0400

    sign in complete with tests

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -3,4 +3,14 @@ FactoryGirl.define do
   factory :ruby_gem do
     sequence(:name) {|n| "Gem #{n}" }
   end
+
+
+end
+
+FactoryGirl.define do
+  factory :user do
+    email "tomcorley@gmail.com"
+    password "password"
+    password_confirmation "password"
+  end
 end
