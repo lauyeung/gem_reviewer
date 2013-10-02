e69fd57b659b57baeaee80862579cb5bcf795ccc
Louise Au Yeung <louise@eabx.org>
2013-10-02 10:21:19 -0400

    removed helper file and added index to migration

---
+++ db/migrate/20131002141849_add_index_to_ruby_gems.rb
@@ -0,0 +1,5 @@
+class AddIndexToRubyGems < ActiveRecord::Migration
+  def change
+    add_index :ruby_gems, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930193014) do
+ActiveRecord::Schema.define(version: 20131002141849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20130930193014) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "ruby_gems", ["name"], name: "index_ruby_gems_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
