78878f6f3a5c046cbc2d02e700f1a4f1cd4fc01f
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-08 11:05:55 -0400

    turned the reviews sort into a method

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20131007221358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
-    t.string   "username", null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
