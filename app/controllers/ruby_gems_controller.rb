ba68ecf0f325845fd2cbd69082b8e0ec1c6bd561
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-10-25 23:50:48 -0400

    User cannot add gem that doesn't exist in the RubyGem API

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'gems'
+
 class RubyGemsController < ApplicationController
   before_filter :authenticate_user!, except: [:show, :index]
 
@@ -5,11 +8,18 @@ class RubyGemsController < ApplicationController
     @ruby_gem = RubyGem.new
   end
 
-  def create
+   def create
+    #BUG - uniqueness is validating "Devise" and "devise"
+    #as two unique gems
     @ruby_gem = RubyGem.new(ruby_gem_params)
-
     if @ruby_gem.save
-      redirect_to new_ruby_gem_path, notice: 'Gem successfully saved!'
+      if ruby_gem_API_query(params) != "This rubygem could not be found."
+        redirect_to new_ruby_gem_path, notice: 'Gem successfully saved!'
+      else
+        redirect_to new_ruby_gem_path, notice: %Q[This ruby gem could not be found. 
+        Please use the following link to check your spelling:
+        <a href="http://rubygems.org/gems">List of Ruby Gems</a>].html_safe
+      end
     else
       render :new
     end
@@ -34,4 +44,9 @@ class RubyGemsController < ApplicationController
     params.require(:ruby_gem).permit(:name)
   end
 
+  def ruby_gem_API_query(params)
+    #BUG - Can't handle spaces
+    @gemname = params[:ruby_gem][:name]
+    Gems.info "#{@gemname}"
+  end
 end
